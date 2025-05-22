9b93724b870fdb5fb85db7acaf3b5c391bdbff5e
Bgopou65 <brigitte@nudred.com>
2025-05-22 16:40:47 -0500

    Add files via upload

---
+++ tidio-chat.js
@@ -0,0 +1,7 @@
+// Tidio Chat Widget
+(function () {
+  var script = document.createElement("script");
+  script.src = "//code.tidio.co/cqijqzdvtrfyt3xtpykbwmbogazvbuzu.js";
+  script.async = true;
+  document.body.appendChild(script);
+})();
